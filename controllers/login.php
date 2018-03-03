4f2ddab3c6fa1153afb83577e1e19181c4629447
Thomas Shaddock <tomshaddock@googlemail.com>
2018-03-03 18:51:35 +0000

    moi

--- controllers/login.php
+++ controllers/login.php
@@ -4,14 +4,14 @@
 class Login extends Controller{
 
 	public function __construct(){
-		var_dump("login");//die;
+		//var_dump("login");//die;
 		parent::__construct();
 		
 	}
 
 	public function index(){
 		//$this->view->msg = 'This is the index, welcome!';
-
+		var_dump("index"); die();
 		$this->view->render('login/index');
 	}
 
